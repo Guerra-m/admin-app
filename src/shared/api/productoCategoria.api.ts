5b6bc61433e3a6ddb6d258028718a9aa1dc2978c
Martin Guerra Local <vernonguerra27@gmail.com>
2026-06-16 13:56:20 -0300

    FIX: bug corregido al eliminar categorias

--- src/shared/api/productoCategoria.api.ts
+++ src/shared/api/productoCategoria.api.ts
@@ -1,33 +1,30 @@
 import { http } from "../api/http";
 
+const BASE = "/api/v1/producto-categorias";
+
 export const addProductToCategory = async (data: {
   producto_id: number;
   categoria_id: number;
   es_principal?: boolean;
 }) => {
-  const res = await http.post("/api/v1/producto-categorias/", data);
+  const res = await http.post(`${BASE}/`, data);
   return res.data;
 };
-
 
 export const removeProductFromCategory = async (data: {
   producto_id: number;
   categoria_id: number;
 }) => {
-  const res = await http.delete("/producto-categorias/", {
-    params: data,
-  });
+  const res = await http.delete(`${BASE}/${data.producto_id}/${data.categoria_id}`);
   return res.data;
 };
-
 
 export const getCategoriesByProduct = async (producto_id: number) => {
-  const res = await http.get(`/producto-categoria/producto/${producto_id}`);
+  const res = await http.get(`${BASE}/producto/${producto_id}`);
   return res.data;
 };
 
-
 export const getProductsByCategory = async (categoria_id: number) => {
-  const res = await http.get(`/producto-categorias/categoria/${categoria_id}`);
+  const res = await http.get(`${BASE}/categoria/${categoria_id}`);
   return res.data;
 };
